eeb705bb30b8fcd8ab5b8933c356da84cc8c8f91
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-02-18 14:40:24 -0600

    App.js update

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ function App() {
       <Header/>
       <div className='main'>
         {
-          movies.map((element) =>{
+          movies.map((element, index) =>{
             return(
               <Movie 
+                key={index}
                 title = {element.Title}
                 year = {element.Year}
                 img = {element.Poster}
